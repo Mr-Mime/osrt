15a1e4eac1987d659fd11f69d91397a6fb1e9845
Mr-Mime <lennart@fabianfisahn.de>
2023-03-16 23:48:13 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'osrt',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
