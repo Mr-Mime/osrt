c091f93c6ad8e05b3151508219f902344f08626c
Mr-Mime <lennart@fabianfisahn.de>
2023-03-20 18:43:44 +0100

    Change AppID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'app.osrt.osrt',
   appName: 'osrt',
   webDir: 'www',
   bundledWebRuntime: false
